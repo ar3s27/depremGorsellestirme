f37383a26eb4e126d78b8801564eb1131cb03fa5
Veysel <veyselerkoyuncu27@gmail.com>
2023-08-10 14:30:18 +0300

    ares

--- earthquake.py
+++ earthquake.py
@@ -2,39 +2,42 @@ import requests as re
 from bs4 import BeautifulSoup as bs
 import time
 
-kutuphane = []
+kutuphane = {}
 
-while(True):
+while True:
     url = "https://deprem.afad.gov.tr/last-earthquakes.html"
-    #sitenin urlsini çekiyorum
     response = re.get(url)
-    soup = bs(response.content,"html.parser")
+    soup = bs(response.content, "html.parser")
+
     table = soup.find("table")
-    #tablo içinden "table"'ı buluyorum
     rows = table.find_all("tr")
 
-    #aldığım veriden 10 tanesini listeliyorum
-    for i,row in enumerate(rows[:10]):
-        cells = row.find_all('td')
+    for i, row in enumerate(rows[:11]):
+        cells = row.find_all("td")
 
-        #6 sutün olan konumu belirleyip değişken içinde atıp ekrana yazdırıyorum
         if cells:
-            location = cells[6].text.strip()
-            magnitude = cells[5].text.strip()
-            latitude = cells[5].text.strip()
-            magnitude = cells[5].text.strip()
-            longitude = cells[3].text.strip()
             date = cells[0].text.strip()
-            id = cells[7].text.strip()
-            
-            if id in kutuphane:
+            latitude = cells[1].text.strip()
+            longitude = cells[2].text.strip()
+            depth = cells[3].text.strip()
+            magnitude = cells[5].text.strip()
+            location = cells[6].text.strip()
+            quake_id = cells[7].text.strip()
+
+            if quake_id in kutuphane:
                 continue
             else:
                 print("Yer: " + location)
                 print("Büyüklük: " + magnitude)
-                print("Derinlik: " + longitude)
+                print("Derinlik: " + depth)
                 print("Tarih: " + date)
                 print("\n")
-                kutuphane.append(id)
+
+                kutuphane[quake_id] = {
+                    "Yer": location,
+                    "Büyüklük": magnitude,
+                    "Derinlik": depth,
+                    "Tarih": date
+                }
 
     time.sleep(3)
