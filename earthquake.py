20624e577f947f542f7f4b52d8e6c3eb58a6b2e0
Veysel <veyselerkoyuncu27@gmail.com>
2023-08-10 14:40:36 +0300

    try catch

--- earthquake.py
+++ earthquake.py
@@ -9,39 +9,43 @@ while True:
     response = re.get(url)
     soup = bs(response.content, "html.parser")
 
-    table = soup.find("table")
-    rows = table.find_all("tr")
+    try:
+        table = soup.find("table")
+        rows = table.find_all("tr")
 
-    for i, row in enumerate(rows[:11]):
-        cells = row.find_all("td")
+        for i, row in enumerate(rows[:11]):
+            cells = row.find_all("td")
 
-        if cells:
-            location = cells[6].text.strip()
-            date = cells[0].text.strip()
-            latitude = cells[1].text.strip()
-            longitude = cells[2].text.strip()
-            depth = cells[3].text.strip()
-            magnitude = cells[5].text.strip()
-            quake_id = cells[7].text.strip()
+            if cells:
+                location = cells[6].text.strip()
+                date = cells[0].text.strip()
+                latitude = cells[1].text.strip()
+                longitude = cells[2].text.strip()
+                depth = cells[3].text.strip()
+                magnitude = cells[5].text.strip()
+                quake_id = cells[7].text.strip()
 
-            if quake_id in kutuphane:
-                continue
-            else:
-                print("Yer: " + location )
-                print("Enlem: " + latitude)
-                print("Boylam: " + longitude)
-                print("Derinlik: " + depth)
-                print("Büyüklük: " + magnitude)
-                print("Tarih: " + date)
-                print("\n")
+                if quake_id in kutuphane:
+                    continue
+                else:
+                    print("Yer: " + location )
+                    print("Enlem: " + latitude)
+                    print("Boylam: " + longitude)
+                    print("Derinlik: " + depth)
+                    print("Büyüklük: " + magnitude)
+                    print("Tarih: " + date)
+                    print("\n")
 
-                kutuphane[quake_id] = {
-                    "location": location,
-                    "latitude": latitude,
-                    "longitude": longitude,
-                    "depth": depth,
-                    "magnitude": magnitude,
-                    "Tarih": date
-                }
+                    kutuphane[quake_id] = {
+                        "location": location,
+                        "latitude": latitude,
+                        "longitude": longitude,
+                        "depth": depth,
+                        "magnitude": magnitude,
+                        "Tarih": date
+                    }
+        time.sleep(60)
+    except AttributeError:
+        print("Table not found. Check the website structure.")
+    
 
-    time.sleep(3)
