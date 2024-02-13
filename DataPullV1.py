f6cfbe4f80412ef0364e9a78fc8d6cf513656303
Veysel <veyselerkoyuncu27@gmail.com>
2024-02-13 14:12:07 +0300

    Update DataPullV1.py

--- DataPullV1.py
+++ DataPullV1.py
@@ -1,4 +1,21 @@
 from fastapi import FastAPI
+import uvicorn
 import requests as re
 from bs4 import BeautifulSoup
 from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="Earthquake")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*']
+)
+
+@app.get('/')
+def earthquake():
+    url = "https://deprem.afad.gov.tr/last-earthquakes.html"
+    response = re.get(url)
+    soup = BeautifulSoup(response.content,"Htlml.parser")
+    return soup
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8800)
