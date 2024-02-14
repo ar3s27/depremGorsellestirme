dec9e5e1c53b1ab74e3d84f3e8e19d905453addd
Veysel <veyselerkoyuncu27@gmail.com>
2024-02-15 02:11:48 +0300

    Update DataPullV1.py

--- DataPullV1.py
+++ DataPullV1.py
@@ -16,46 +16,52 @@ kutuphane = {}
 
 @app.get('/')
 def earthquake():
-    url = "https://deprem.afad.gov.tr/last-earthquakes.html"
-    response = re.get(url)
-    
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        table = soup.find("table")
-        rows = table.find_all("tr")[1:]  # Skip the first row (header)
-        last_10_rows = rows[:10]  # Get the last 10 rows
-        
-        earthquakes = {}
-        for row in last_10_rows:
-            cells = row.find_all("td")
-
-            if cells:
-                location = cells[6].text.strip()
-                date = cells[0].text.strip()
-                latitude = cells[1].text.strip()
-                longitude = cells[2].text.strip()
-                depth = cells[3].text.strip()
-                magnitude = cells[5].text.strip()
-                quake_id = str(uuid.uuid4())
+    while True:
+        url = "https://deprem.afad.gov.tr/last-earthquakes.html"
+        response = re.get(url)
+        try:
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, "html.parser")
+                table = soup.find("table")
+                rows = table.find_all("tr")[1:]  # Skip the first row (header)
+                last_10_rows = rows[:10]  # Get the last 10 rows
                 
-                earthquake_data = {
-                    "quake_id": quake_id,
-                    "location": location,
-                    "latitude": latitude,
-                    "longitude": longitude,
-                    "depth": depth,
-                    "magnitude": magnitude,
-                    "date": date
-                }
-                earthquakes[quake_id] = earthquake_data
-        
-        # Update the global dictionary with new earthquake data
-        kutuphane.update(earthquakes)
-        
-        return earthquakes
-    else:
-        # If there's an error fetching the page, return an error message
-        return {"error": "Failed to fetch earthquake data"}
+                earthquakes = {}
+                for row in last_10_rows:
+                    cells = row.find_all("td")
 
+                    if cells:
+                        location = cells[6].text.strip()
+                        date = cells[0].text.strip()
+                        latitude = cells[1].text.strip()
+                        longitude = cells[2].text.strip()
+                        depth = cells[3].text.strip()
+                        magnitude = cells[5].text.strip()
+                        quake_id = str(uuid.uuid4())
+                        if quake_id in kutuphane:
+                            continue
+                        else: 
+                            earthquake_data = {
+                                "quake_id": quake_id,
+                                "location": location,
+                                "latitude": latitude,
+                                "longitude": longitude,
+                                "depth": depth,
+                                "magnitude": magnitude,
+                                "date": date
+                            }
+                            earthquakes[quake_id] = earthquake_data
+                
+                # Update the global dictionary with new earthquake data
+                kutuphane.update(earthquakes)
+                
+                return earthquakes
+            else:
+                # If there's an error fetching the page, return an error message
+                return {"error": "Failed to fetch earthquake data"}
+
+        except AttributeError:
+            print("Table not found. Check the website structure.")
+ 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8800)
